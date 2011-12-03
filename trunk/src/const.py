d1784bf7d0b3930f5d337fca831a7bc32e2b1589
matyk@bk.ru <matyk@bk.ru@46a4f1c8-1171-db67-c0b3-220ec9584f44>
2011-12-03 13:03:22 +0000

    Added const.py Rewrited Access names

---
+++ trunk/src/const.py
@@ -0,0 +1,23 @@
+
+
+ACTIONS =  {101 : 'Close',
+            102 : 'Click',
+            103 : 'ClickInput',
+            104 : 'CloseClick',
+            105 : 'DoubleClick',
+            106 : 'DoubleClickInput',
+            107 : 'DragMouse',
+            108 : 'DrawOutline',
+            109 : 'Maximize',
+            110 : 'Minimize',
+            111 : 'MoveMouse',
+            112 : 'MoveWindow',
+            113 : 'PressMouse',
+            114 : 'PressMouseInput',
+            115 : 'ReleaseMouse',
+            116 : 'ReleaseMouseInput',
+            117 : 'Restore',
+            118 : 'RightClick',
+            119 : 'RightClickInput',
+            120 : 'SetFocus',
+            }
