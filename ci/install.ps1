108fcec63804955af903d6e053cdbac5432be6c1
Denis Matiychuk <moden.py@yandex.ru>
2015-09-29 14:03:49 +0300

    Fix#41 Add Texts property for TabControl's item. Add dependencies folder. Move wx wheel files here. Increment version.

--- ci/install.ps1
+++ ci/install.ps1
@@ -2,9 +2,9 @@
 # Authors: Denis Matiychuk
 # License: CC0 1.0 Universal: http://creativecommons.org/publicdomain/zero/1.0/
 
-$WX_32_URL = "http://www.lfd.uci.edu/~gohlke/pythonlibs/3i673h27/wxPython-3.0.2.0-cp27-none-win32.whl"
-$WX_64_URL = "http://www.lfd.uci.edu/~gohlke/pythonlibs/3i673h27/wxPython-3.0.2.0-cp27-none-win_amd64.whl"
-$WX_COMMON_URL = "http://www.lfd.uci.edu/~gohlke/pythonlibs/3i673h27/wxPython_common-3.0.2.0-py2-none-any.whl"
+$WX_32 = "./deps/wxPython-3.0.2.0-cp27-none-win32.whl"
+$WX_64 = "./deps/wxPython-3.0.2.0-cp27-none-win_amd64.whl"
+$WX_COMMON = "./deps/wxPython_common-3.0.2.0-py2-none-any.whl"
 
 
 function InstallWX ($python_version, $architecture, $python_home) {
@@ -12,11 +12,11 @@ function InstallWX ($python_version, $architecture, $python_home) {
 	$pip_path = $python_home + "\Scripts\pip.exe"
 
     if ($architecture -eq "32") {
-        $wx_wheel = $WX_32_URL
+        $wx_wheel = $WX_32
     } else {
-        $wx_wheel = $WX_64_URL
+        $wx_wheel = $WX_64
     }
-	$args = "install " + $wx_wheel + " " + $WX_COMMON_URL
+	$args = "install " + $wx_wheel + " " + $WX_COMMON
 	Write-Host "Install wheels" $args "by" $pip_path
     Start-Process -FilePath $pip_path -ArgumentList $args -Wait -Passthru
 }
